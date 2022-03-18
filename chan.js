1cb5d6fad1feb9db5b7c7da4cfd1aafafb39f44f
Shahad Alaskah <56152970+ShahadAlaskah@users.noreply.github.com>
2022-03-18 22:09:12 +0300

    Update chan.js

--- chan.js
+++ chan.js
@@ -1,73 +1,48 @@
+const showPessimistOrFoggyWarnaing = () => {
+  if (fogIndexs.includes(positionOfNextRoom)) {
+    alert("warning!! fooooog");
+  }
+  if (pssIndexs.includes(positionOfNextRoom)) {
+    alert("warning!! passsssssssssss");
+    //endGame(1);
+  }
+};
 const moveRDLU = () => {
-    // if (
-    //   tapeIndexs.includes(positionOfNextRoom) &&
-    //   numberOfTape != 3 &&
-    //   myPath.includes(positionOfNextRoom) == false
-    // ) {
-    //   setnumberOfTape(numberOfTape + 1);
-    // } else if (tapeIndexs.includes(positionOfNextRoom) && numberOfTape == 3) {
-    //   setPoint(Point + 3);
-    // }
-    if (positionOfNextRoom == roomNum + 1) {
-      //moveR
-      if ((roomNum - 5) % 6 != 0) {
-        //showPessimistOrFoggyWarnaing(1);
-        setRoomNum(roomNum + 1);
-        //setMyPath([...myPath, roomNum + 1]);
-        // for (let index = 0; index < aa.length; index++) {
-        //   if (aa[index] != 1) {
-        //     if (!myPath.includes(roomNum + aa[index])) {
-        //       board[roomNum + aa[index]] = close;
-        //     }
-        //   }
-        // }
-      }
-    } else if (positionOfNextRoom == roomNum - 1) {
-      //moveL
-      if (roomNum % 6 != 0) {
-        //showPessimistOrFoggyWarnaing(-1);
-        setRoomNum(roomNum - 1);
-        // setMyPath([...myPath, roomNum - 1]);
-        // for (let index = 0; index < aa.length; index++) {
-        //   if (aa[index] != -1) {
-        //     if (!myPath.includes(roomNum + aa[index])) {
-        //       board[roomNum + aa[index]] = close;
-        //     }
-        //   }
-        // }
-      }
-    } else if (positionOfNextRoom == roomNum - 6) {
-      //moveUp
-      if (roomNum > 5) {
-        //showPessimistOrFoggyWarnaing(-6);
-        setRoomNum(roomNum - 6);
-        // setMyPath([...myPath, roomNum - 6]);
-        // for (let index = 0; index < aa.length; index++) {
-        //   if (aa[index] != -6) {
-        //     if (!myPath.includes(roomNum + aa[index])) {
-        //       board[roomNum + aa[index]] = close;
-        //     }
-        //   }
-        // }
-      }
-    } else if (positionOfNextRoom == roomNum + 6) {
-      //moveD
-      if (roomNum < 30) {
-        //showPessimistOrFoggyWarnaing(6);
-        setRoomNum(roomNum + 6);
-        // setMyPath([...myPath, roomNum + 6]);
-        // for (let index = 0; index < aa.length; index++) {
-        //   if (aa[index] != 6) {
-        //     if (!myPath.includes(roomNum + aa[index])) {
-        //       board[roomNum + aa[index]] = close;
-        //     }
-        //   }
-        // }
-      }
+  // if (
+  //   tapeIndexs.includes(positionOfNextRoom) &&
+  //   numberOfTape != 3 &&
+  //   myPath.includes(positionOfNextRoom) == false
+  // ) {
+  //   setnumberOfTape(numberOfTape + 1);
+  // } else if (tapeIndexs.includes(positionOfNextRoom) && numberOfTape == 3) {
+  //   setPoint(Point + 3);
+  // }
+  if (positionOfNextRoom == roomNum + 1) {
+    //moveR
+    if ((roomNum - 5) % 6 != 0) {
+      showPessimistOrFoggyWarnaing();
+      setRoomNum(roomNum + 1);
     }
-    //myPath.push(positionOfNextRoom);
-    // board[positionOfNextRoom] = board1[positionOfNextRoom];
-    // if (roomNum == 35) {
-    //   endGame(3);
-    // }
-  };
+  } else if (positionOfNextRoom == roomNum - 1) {
+    //moveL
+    if (roomNum % 6 != 0) {
+      showPessimistOrFoggyWarnaing();
+      setRoomNum(roomNum - 1);
+    }
+  } else if (positionOfNextRoom == roomNum - 6) {
+    //moveUp
+    if (roomNum > 5) {
+      showPessimistOrFoggyWarnaing();
+      setRoomNum(roomNum - 6);
+    }
+  } else if (positionOfNextRoom == roomNum + 6) {
+    //moveD
+    if (roomNum < 30) {
+      showPessimistOrFoggyWarnaing();
+      setRoomNum(roomNum + 6);
+    }
+  }
+  // if (roomNum == 35) {
+  //   endGame(3);
+  // }
+};
